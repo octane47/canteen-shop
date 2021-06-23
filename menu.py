8f72c6e083fca0ef6aa254cb21a83fc37862925e
Nikole Hunter <80729663+octane47@users.noreply.github.com>
2021-06-23 22:43:55 +0000

    added main code for yes or no question still a bit to go but getting there nothless

--- menu.py
+++ menu.py
@@ -1,10 +1,3 @@
-
-
-# Funtions go here...
-name=str(input("whats your name: "))
-print ("kia ora", name)
-print ("this is the fraser high school canteen, would you like to see our menu")
-
 def yes_no(question):
     vaild = False
     while not vaild:
@@ -23,3 +16,20 @@ def yes_no(question):
         else:
             print("please answer yes / no")
 
+  def yes_no(question):
+    vaild = False
+    while not vaild:
+        response = input(question).lower()
+
+        if response == "yes" or response == "y":
+            response = "yes "
+            return response
+            print(" we currently only have pies and burgers in store")
+            print("burgers are $7.89 and pies are $4.50")
+
+        if response == "no" or response == "n":
+            response = "no"
+            return response
+
+        else:
+            print("please answer yes / no")
